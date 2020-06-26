bfa9063d422defe9298bc5b008bdd73921c5e27f
Lev Stambler <levst@google.com>
2020-06-26 07:56:05 -0400

    clean up

--- biogrid/apps/api/src/app/config/ErrorHandler.ts
+++ biogrid/apps/api/src/app/config/ErrorHandler.ts
@@ -17,23 +17,31 @@ export class ApiError extends Error implements ErrorType {
   constructor(errorType: ErrorType) {
     super(errorType.message);
     this.name = errorType.name;
-    if (errorType.statusCode) this.statusCode = errorType.statusCode;
+    if (errorType.statusCode) {
+      this.statusCode = errorType.statusCode;
+    }
     this.fields = errorType.fields;
   }
 }
 
 export class ErrorHandler {
-  public static handleError(error: ApiError, req: Request, res: Response, next: NextFunction): void {
+  public static handleError(
+    error: ApiError,
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): void {
     const { name, message, fields, statusCode } = error;
     Logger.error(
       `Error: ${statusCode}`,
       `Error Name: ${name}`,
       `Error Message: ${message}`,
-      'Error Fields:', fields || {},
-      'Original Error: ', error
+      'Error Fields:',
+      fields || {},
+      'Original Error: ',
+      error
     );
     res.status(statusCode).json({ name, message, fields });
     next();
   }
-
 }
