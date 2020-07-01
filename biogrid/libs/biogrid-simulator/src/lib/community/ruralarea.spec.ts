ad4e20d73534dbc156dd31ee947c6ceca3a85717
Awad <awado@google.com>
2020-07-01 18:04:20 -0400

    Add tests for ruralarea class.

--- biogrid/libs/biogrid-simulator/src/lib/community/ruralarea.spec.ts
+++ biogrid/libs/biogrid-simulator/src/lib/community/ruralarea.spec.ts
@@ -1,55 +1,22 @@
 import { Building } from '../building';
 import { RuralArea } from './';
 
-/*
+
 describe('tests for RuralArea class', () => {
-  
-  test('negative initial energy', () => {
-    expect(() => new Building(-3)).toThrow(
-      `Can't create a building with negative energy!`
-    );
+  test('getBuildingById()', () => {
+    const buildings : Building[] = [new Building(5), new Building(10), new Building(20)];
+    const ruralarea = new RuralArea(buildings);
+    const aBuildingId = buildings[1].getBuildingId();
+    expect(ruralarea.getBuildingById(aBuildingId)).toEqual(buildings[1]);
   });
 
-  test('negative input for increaseEnergy()', () => {
-    const building = new Building(5);
-    expect(() => building.increaseEnergy(-5)).toThrow(
-      `Can't add negative energy!`
-    );
+  test('addBuilding()', () => {
+    const buildings : Building[] = [new Building(5), new Building(10), new Building(20)];
+    const ruralarea = new RuralArea(buildings);
+    const newBuilding = new Building(40);
+    ruralarea.addBuilding(newBuilding)
+    expect(ruralarea.getBuildings().length).toEqual(4);
   }); 
-
-  test('increaseEnergy()', () => {
-    const building = new Building(5);
-    building.increaseEnergy(5);
-    const totalEnergy = 10;
-    expect(building.getEnergyInJoules()).toEqual(totalEnergy);
-  });
-
-  test('negative input for decreaseEnergy()', () => {
-    const building = new Building(5);
-    expect(() => building.decreaseEnergy(-5)).toThrow(
-      `Can't use a negative amount of energy!`
-    );
-  }); 
-
-  test('decreaseEnergy() with input > current energy', () => {
-    const building = new Building(5);
-    building.decreaseEnergy(80);
-    const totalEnergy = 0;
-    expect(building.getEnergyInJoules()).toEqual(totalEnergy);
-  });
-
-  test('decreaseEnergy()', () => {
-    const building = new Building(5);
-    building.decreaseEnergy(4);
-    const totalEnergy = 1;
-    expect(building.getEnergyInJoules()).toEqual(totalEnergy);
-  });
-
-  test('getters and setters for buildingId', () => {
-    const building = new Building(5);
-    building.setBuildingId(2);
-    expect(building.getBuildingId()).toEqual(2);
-  });
 
 });
-*/
+
