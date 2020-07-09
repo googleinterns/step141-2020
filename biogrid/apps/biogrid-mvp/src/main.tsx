00294f8b1d3ab23c62f4ed9451ea43cf6af89bbe
Lev Stambler <levst@google.com>
2020-07-09 15:11:52 -0400

    deploy

--- biogrid/apps/biogrid-mvp/src/main.tsx
+++ biogrid/apps/biogrid-mvp/src/main.tsx
@@ -3,4 +3,10 @@ import ReactDOM from 'react-dom';
 
 import App from './app/app';
 
+// declare let GlobalFetch;
+declare module 'node-fetch' {
+    const fetch: GlobalFetch['fetch'];
+    export default fetch;
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));
