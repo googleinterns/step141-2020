4ae50dcbf9aa0ce03e0d165fc310244baacd9422
Lev Stambler <levst@google.com>
2020-07-14 14:41:59 -0400

    begin battery disp

--- biogrid/libs/biogrid-simulator/src/lib/biogrid/biogrid-class.ts
+++ biogrid/libs/biogrid-simulator/src/lib/biogrid/biogrid-class.ts
@@ -3,17 +3,24 @@ import {
   GridAction,
   GridOptions,
   Town,
+  TownSize,
+  ItemPosition,
 } from '@biogrid/grid-simulator';
 import { BiogridState } from '../biogrid-state';
+import { BioBattery } from '../biobattery';
 
 export interface BiogridOptions extends GridOptions {
-  numberOfSmallBatteryCells?: number;
-  numberOfLargeBatteryCells?: number;
+  numberOfSmallBatteryCells: number;
+  numberOfLargeBatteryCells: number;
 }
 
 export class Biogrid implements Grid {
   private state: BiogridState;
-  constructor(town: Town, opts?: BiogridOptions) {
+  constructor(town: Town, opts: BiogridOptions) {
+    const smallBatteryPositions = this.createBatteryPositions(town.getTownSize(), opts.numberOfSmallBatteryCells);
+    const largeBatteryPositions = this.createBatteryPositions(town.getTownSize(), opts.numberOfLargeBatteryCells);
+    // TODO constants
+    const smallBatteries = smallBatteryPositions.map(pos => new BioBattery(pos.x, pos.y, 0, 100))
     this.state = new BiogridState([]);
   }
 
@@ -26,4 +33,20 @@ export class Biogrid implements Grid {
     return;
   }
 
+  /**
+   * A simplified algorithm to (mostly) evenly space out batteries throughout the square town
+   * Split the town into rows and columns and then place a battery in the center of each cell
+   */
+  private createBatteryPositions(townSize: TownSize, numberOfBatteries: number): ItemPosition[] {
+    const cols = Math.ceil(numberOfBatteries / townSize.width);
+    const rows = numberOfBatteries / cols;
+    const positions: ItemPosition[] = [];
+    for (let i = 0; i < numberOfBatteries; i++) {
+      positions.push({
+        x: ((numberOfBatteries % cols + 0.5) / cols) * townSize.width,
+        y: ((Math.floor(i / cols) + 0.5) / rows) * townSize.height
+      })
+    }
+    return positions
+  }
 }
