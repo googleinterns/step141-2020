00956406c3b67bed600fb967fd9b6d7fac819f9c
Roland Naijuka <rnaijuka@google.com>
2020-07-14 16:58:18 -0400

    Create non-charged batteries batteriesSigned-off-by: Roland Naijuka <rnaijuka@google.com>

--- biogrid/libs/biogrid-simulator/src/lib/biogrid/biogrid-class.ts
+++ biogrid/libs/biogrid-simulator/src/lib/biogrid/biogrid-class.ts
@@ -5,6 +5,8 @@ import {
   Town,
   TownSize,
   ItemPosition,
+  Energy,
+  Battery,
 } from '@biogrid/grid-simulator';
 import { BiogridState } from '../biogrid-state';
 import { BioBattery } from '../biobattery';
@@ -16,16 +18,41 @@ export interface BiogridOptions extends GridOptions {
 
 export class Biogrid implements Grid {
   private state: BiogridState;
+
+  // All details for the batteries in the grid
+  // The small batteries in the grid, will approximately have a maxCapacity of 13,500KJ
+  private smallBatteries: Battery[];
+  private smallMaxCapacity = 13500;
+  // The large batteries in the grid, will approximately have a maxCapacity of 540,000KJ
+  private largeBatteries: Battery[] = [];
+  private largeMaxCapacity = 13500;
+  // Initial energy for all the batteries is set to 0KJ
+  private initialCapacity = 0;
+
+  // All details for the houses / energyUsers in the grid
+  private townsize: TownSize;
+
+  
   constructor(town: Town, opts: BiogridOptions) {
     const smallBatteryPositions = this.createBatteryPositions(town.getTownSize(), opts.numberOfSmallBatteryCells);
     const largeBatteryPositions = this.createBatteryPositions(town.getTownSize(), opts.numberOfLargeBatteryCells);
     // TODO constants
-    const smallBatteries = smallBatteryPositions.map(pos => new BioBattery(pos.x, pos.y, 0, 100))
+    this.smallBatteries = this.createSmallBatteries(smallBatteryPositions);
+    this.largeBatteries = this.createLargeBatteries(largeBatteryPositions);
+
     this.state = new BiogridState([]);
   }
 
   getSystemState(): BiogridState {
     return this.state;
+  }
+
+  private createSmallBatteries(positions: ItemPosition[]): Battery[] {
+    return positions.map(position => new BioBattery(position.x, position.y, this.initialCapacity, this.smallMaxCapacity));
+  }
+
+  private createLargeBatteries(positions: ItemPosition[]): Battery[] {
+    return positions.map(position => new BioBattery(position.x, position.y, this.initialCapacity, this.largeMaxCapacity));
   }
 
   takeAction(action: GridAction) {
