4655463e413a17532c1e9874ad0866d7a83ad016
Roland Naijuka <rnaijuka@google.com>
2020-07-14 17:25:32 -0400

    add the towns
    Signed-off-by: Roland Naijuka <rnaijuka@google.com>

--- biogrid/libs/biogrid-simulator/src/lib/biogrid/biogrid-class.ts
+++ biogrid/libs/biogrid-simulator/src/lib/biogrid/biogrid-class.ts
@@ -10,6 +10,9 @@ import {
 } from '@biogrid/grid-simulator';
 import { BiogridState } from '../biogrid-state';
 import { BioBattery } from '../biobattery';
+import { Building } from '../building';
+import { GridItem } from 'libs/grid-simulator/src/lib/grid-item';
+import { BioEnergySource } from '../bioenergy-source';
 
 export interface BiogridOptions extends GridOptions {
   numberOfSmallBatteryCells: number;
@@ -24,23 +27,45 @@ export class Biogrid implements Grid {
   private smallBatteries: Battery[];
   private smallMaxCapacity = 13500;
   // The large batteries in the grid, will approximately have a maxCapacity of 540,000KJ
-  private largeBatteries: Battery[] = [];
+  private largeBatteries: Battery[];
   private largeMaxCapacity = 13500;
   // Initial energy for all the batteries is set to 0KJ
   private initialCapacity = 0;
 
   // All details for the houses / energyUsers in the grid
   private townsize: TownSize;
+  private town: Town;
+
+  // All details for the source of energy
+  private solarPanels: BioEnergySource[];
 
   
   constructor(town: Town, opts: BiogridOptions) {
+    // Batteries
     const smallBatteryPositions = this.createBatteryPositions(town.getTownSize(), opts.numberOfSmallBatteryCells);
     const largeBatteryPositions = this.createBatteryPositions(town.getTownSize(), opts.numberOfLargeBatteryCells);
     // TODO constants
     this.smallBatteries = this.createSmallBatteries(smallBatteryPositions);
     this.largeBatteries = this.createLargeBatteries(largeBatteryPositions);
 
-    this.state = new BiogridState([]);
+    // Towns
+    this.townsize = {height: town.getTownSize().height, width: town.getTownSize().width};
+    this.town = town;
+
+    // Enery Source
+    // TODO implement the solar panels
+    this.solarPanels = [];
+    
+    this.state = new BiogridState(this.createGridItems());
+  }
+
+  private createGridItems(): GridItem[] {
+    let gridItems: GridItem[] = [];
+    this.smallBatteries.map(battery => gridItems.push(battery));
+    this.largeBatteries.map(battery => gridItems.push(battery));
+    this.town.getEnergyUsers().map(energyUser => gridItems.push(energyUser));
+
+    return gridItems;
   }
 
   getSystemState(): BiogridState {
