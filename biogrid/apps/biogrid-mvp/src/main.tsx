a648fdebc80434c1a4f752536c35812f1a472d69
Lev Stambler <levst@google.com>
2020-07-14 15:07:17 -0400

    remove declare global

--- biogrid/apps/biogrid-mvp/src/main.tsx
+++ biogrid/apps/biogrid-mvp/src/main.tsx
@@ -3,10 +3,4 @@ import ReactDOM from 'react-dom';
 
 import App from './app/app';
 
-declare global {
-  interface GlobalFetch {
-    [k: string]: any;
-  }
-}
-
 ReactDOM.render(<App />, document.getElementById('root'));
