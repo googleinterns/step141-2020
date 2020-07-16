5ecf6c685984d032fc8f32dd6c371197ac81fec4
Roland Naijuka <rnaijuka@google.com>
2020-07-16 10:56:14 -0400

    add constants and export the bioEnergySource
    Signed-off-by: Roland Naijuka <rnaijuka@google.com>

--- biogrid/libs/biogrid-simulator/src/index.ts
+++ biogrid/libs/biogrid-simulator/src/index.ts
@@ -1,5 +1,6 @@
 export * from './lib/biobattery';
 export * from './lib/biobrain';
+export * from './lib/bioenergy-source';
 export * from './lib/biogrid';
 export * from './lib/biogrid-action';
 export * from './lib/biogrid-state';
--- biogrid/libs/biogrid-simulator/src/lib/config/bio-constants.ts
+++ biogrid/libs/biogrid-simulator/src/lib/config/bio-constants.ts
@@ -7,3 +7,26 @@ export const LARGE_BATTERY = {
   DEFAULT_START_ENERGY: 540000,
   MAX_CAPACITY: 540000,
 };
+
+// Each day a solar panel is roughly charged with electricity
+// for 4hours at a rate of 250 watts ~ 1000watts ~~ 3.6Mj
+// To prolong their life time, solar panels will be expected 
+// to keep a minimum of about 10000j
+export const SOLAR_PANEL = {
+  DEFAULT_INITIAL_ENERGY: 3600000,
+  MIN_CAPACITY: 10000,
+};
+
+export const enum GRID_ITEM_NAMES {
+  SOLAR_PANEL = 'solar_panel',
+  SMALL_BATTERY = 'small_battery',
+  LARGE_BATTERY = 'large_battery',
+  ENERGY_USER = 'energy_user',
+  GRID = 'grid',
+};
+
+export const BUILDING = {
+  DEFAULT_INITIAL_ENERGY: 4545000,
+  MIN_CAPACITY: 0,
+  MAX_CAPACITY: 4545000,
+};
