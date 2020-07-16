f26cca033da66de94b673a5f0af7fde74bc262a8
Awad <awado@google.com>
2020-07-16 17:51:19 -0400

    Simplify amount of clicks to result page.

--- biogrid/apps/biogrid-mvp/src/app/pages/simulate-page/simulate-page.tsx
+++ biogrid/apps/biogrid-mvp/src/app/pages/simulate-page/simulate-page.tsx
@@ -3,42 +3,42 @@ import './simulate-page.css';
 import { Client } from '../../client';
 import { BiogridSimulationResults } from '../../build';
 
+/*
 enum SimulationStatus {
   NOT_STARTED,
   RUNNING,
   RAN,
 }
+*/
 
 export const SimulatePage = () => {
   const [simulationResults, setSimulationResults] = useState<
     BiogridSimulationResults
   >();
+  /*
   const [simulationStatus, setSimulationStatus] = useState(
     SimulationStatus.NOT_STARTED
   );
-
+  */
   const client = Client.getInstance();
+  /*
   async function runSimulation() {
     setSimulationStatus(SimulationStatus.RUNNING);
     await client.api.runBiogridSimulation();
     setSimulationStatus(SimulationStatus.RAN);
   }
+  */
   async function getSimulationResults() {
+    //setSimulationStatus(SimulationStatus.RUNNING);
+    await client.api.runBiogridSimulation();
+    //setSimulationStatus(SimulationStatus.RAN);
     const results = await client.api.getBiogridSimulationResults();
     setSimulationResults(results);
   }
 
   return (
     <div className="simulation">
-      <button onClick={() => runSimulation()}>Run the simulation</button>
-      {simulationStatus === SimulationStatus.RUNNING && (
-        <div>Loading your results...</div>
-      )}
-      {simulationStatus === SimulationStatus.RAN && (
-        <button onClick={() => getSimulationResults()}>
-          Get simulation results
-        </button>
-      )}
+      <button onClick={() => getSimulationResults()}>Get simulation results</button>
       {simulationResults && (
         <div className="results">
           <table>
