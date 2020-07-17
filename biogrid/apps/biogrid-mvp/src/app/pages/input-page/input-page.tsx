0ecee22dc724a90b3fed28ae06b78410587d172e
Awad <awado@google.com>
2020-07-17 10:30:50 -0400

    Alphabetize css styling files and give the visual an id.

--- biogrid/apps/biogrid-mvp/src/app/pages/input-page/input-page.tsx
+++ biogrid/apps/biogrid-mvp/src/app/pages/input-page/input-page.tsx
@@ -46,7 +46,7 @@ export const InputPage = () => {
     <div className="input-page">
       <form onSubmit={(e: React.SyntheticEvent<EventTarget>) => onSubmit(e)}>
       <h1>Welcome to the Biogrid Simulator!</h1>
-      <img src={Visual}></img>
+      <img src={Visual} id="biogridVisual"></img>
       <p>This website will simulate how a <a href="https://www.sciencedirect.com/science/article/pii/S136403211830128X" target="_blank">microgrid</a> can 
         optimize energy use in a community by using <a href="https://biomimicry.org/what-is-biomimicry/" target="_blank">biomimicry</a> to 
         mimic the natural energy storage of the human body given a range of sunlight data and
