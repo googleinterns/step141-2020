1c0dc0afc2609d405b204e97c0e6af4b6c7c4e66
Awad <awado@google.com>
2020-07-21 12:16:02 -0400

    Update home page description to include town size.

--- biogrid/apps/biogrid-mvp/src/app/pages/input-page/input-page.tsx
+++ biogrid/apps/biogrid-mvp/src/app/pages/input-page/input-page.tsx
@@ -62,7 +62,7 @@ export const InputPage = () => {
       <p>This website will simulate how a <a href="https://www.sciencedirect.com/science/article/pii/S136403211830128X" target="_blank">microgrid</a> can 
         optimize energy use in a community by using <a href="https://biomimicry.org/what-is-biomimicry/" target="_blank">biomimicry</a> to 
         mimic the natural energy storage of the human body given a range of sunlight data, number of small and large 
-        battery cells, buildings, and solar panels. In the <b>visual</b> above, the <b>large battery cells</b> are 
+        battery cells, buildings, solar panels, and town size. In the <b>visual</b> above, the <b>large battery cells</b> are 
         centered in the <b>middle of the town</b> so our grid can take energy from it in the event that
         no energy is coming from our renewable sources. Outside of our large battery cells, we have <b>small 
         battery cells</b> spread <b>throughout the town</b> so that our buildings can have an
