6d92eebe58c4a73a65751780ebbfc7b5b9c4a1f9
Roland Naijuka <rnaijuka@google.com>
2020-07-23 11:22:33 -0400

    Make changes in the comments that describe functions
    Signed-off-by: Roland Naijuka <rnaijuka@google.com>

--- biogrid/libs/biogrid-simulator/src/lib/config/efficiency.ts
+++ biogrid/libs/biogrid-simulator/src/lib/config/efficiency.ts
@@ -2,44 +2,55 @@ import { RESISTANCE } from ".";
 import { Power } from '@biogrid/grid-simulator';
 /**
  * This function calculates the resistance of a given length of WIRE 16 AWG which is the one we are using for our transmission
+ * The wire has a constant reistance per kilometre, thus multiplying by the length of the wire gives 
+ * the resistance of that whole wire
  * @param length the length of the wire which is synonymous to the distance between the supplier and reciever of power
  * @returns the resistance of the wire for calculate the power loss
  */
-export function calculateResistance(length: number, isMetre: boolean = true) {
-  if (!isMetre) {
+export function calculateResistance(length: number, isMetre: boolean = false) {
+  // Change the metre values to kilometres
+  if (isMetre) {
     length = length / 1000;
   }
-  return RESISTANCE.RESISTANCE_16 * length;
+  return (RESISTANCE.RESISTANCE_16 * length);
 }
 
 /**
- * This function calculates the current passing through the circuit at any given time
+ * This function calculates the current passing through the circuit at any given time using the physics 
+ * equation of calculating current, I = V / R where I is the current, V is voltage, R is resistance
  * @param voltage is the voltage of the circuit at that particular point
  * @param resistance is the resistance of the wires and any connected materials in the circuit
  * @returns the current flowing in that particular circuit
  */
 export function calculateCurrent(voltage: number, loadResistance: number, wireResistance: number) {
-  return voltage / (loadResistance + wireResistance);
+  return (voltage / (loadResistance + wireResistance));
 }
 
 /**
- * This function calculates the voltage loss while passing through the wires
+ * This function calculates the voltage loss while passing through the wires using the physics 
+ * equation of calculating voltage. V = I * R where V is voltage, I is current and R is resistance
  * @param current is the current in the circuit
  * @param resistance is the resistance of the wires
  * @returns the voltage lost while transport through the wires
  */
 export function calculateVoltage(current: number, resistance: number) {
-  return current * resistance;
+  return (current * resistance);
 }
 
 /**
  * This function calculates the power in the circuit for usage in the efficiency
+ * It calculates this based on the two physics equations
+ *    1. P = V * I
+ *    2. I = V / R
+ * where P is power, V is voltage, I is current and R is resistance
+ * Since current is constant in circuits
+ * Th final equation used is P = V^2 / R
  * @param voltage is the voltage in the circuit at that particular time
  * @param resistance is the resistance of the wires
  * @return the power transport over the given @param resistance
  */
 export function calculatePower(voltage: number, resistance: number) {
-  return voltage * voltage / resistance;
+  return (Math.pow(voltage, 2) / resistance);
 }
 
 /**
@@ -53,7 +64,10 @@ export function calculateVoltageFromPower(power: number, resistance: number) {
 }
 
 /**
- * This function calculates the efficiency of the circuit or system
+ * This function calculates the efficiency of the circuit or system.
+ * It bases this from the physics equation for calculting efficiency in a circuit
+ * Efficiency = P_output / P_input * 100%
+ * where P_output is the power output of the circuit, and P_input is the power input into that same circuit
  * @param input is the input Power at the beginning of the circuit
  * @param output is the output power at the end of the the circuit
  * @returns the efficiency of the system which is in percentages
