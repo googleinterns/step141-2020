6e14e10bb9481679daed4d87f283bf69fe26aa73
Awad <awado@google.com>
2020-07-28 11:15:32 -0400

    Clarifies info and fixes grammatical issues.

--- biogrid/apps/biogrid-mvp/src/app/pages/info-page/info-page.tsx
+++ biogrid/apps/biogrid-mvp/src/app/pages/info-page/info-page.tsx
@@ -11,16 +11,16 @@ export const InfoPage = () => {
 
       <img src={Visual} id="EnergyFlow"></img>
 
-      <p>Firstly, the grid items <b>(batteries(b), solar panels(s), and buildings(b))</b> will be randomly placed in the town. 
-      The grid items include the <b>grid(g)</b> itself as well. In case of small batteries, we will refer to them as <b>s_b</b>, and large batteries as <b>l_b</b>.
+      <p>Firstly, the grid items <b>(small batteries(s_b), large batteries(l_b) solar panels(s), and buildings(B))</b> will be randomly placed in the town. 
+      The grid items include the <b>grid(g)</b> itself as well.
       </p>
 
-      <p>Then they will be connected by a <b>directed graph G</b>, which connects the grid items from the grid to these items except the solar panels i.e <b>G(g, b), G(g, b)</b>. 
-      Also the grid items are connected to one another. For instance the small batteries are connected to the buildings <b>G(s_b, b)</b>, the buildings are connected to one another <b>G(b_0, b_1)</b>. 
-      The batteries, and solar panels are connected to the grid i.e <b>G(b, g), G(s, g)</b>.
+      <p>Then the grid items will be connected by a <b>directed graph G</b>, which connects them from the <b>grid</b> to each item except the solar panels i.e <b>G(g, s_b), G(g, l_b), G(g, B)</b>. 
+      Also some of the grid items are connected to one another. For instance the small batteries are connected to the buildings <b>G(s_b, B)</b>, the buildings are connected to one another <b>G(B_0, B_1)</b>. 
+      The batteries, and solar panels are connected to the grid i.e <b>G(s_b, g), G(l_b, g), G(s, g)</b>.
       </p>
 
-      <p>We then calculate the shortest distances between the grid items if we are to transfer power from supplier to receiver of that power using <b>Djiskra’s algorithm</b>. 
+      <p>We then calculate the shortest distances between the grid items if we are to transfer power from supplier to receiver of that power using <b>Djisktra’s algorithm</b>. 
       Power is then transferred and the changes are made to the receiver and supplier. In this version, we are not considering the priorities of sending power. 
       We are considering the supplier which is close to the receiver. We are also implementing the process of giving power to houses as one where a house is given all the power it requires 
       or zero if there is not enough for it.
