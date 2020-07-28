d93029838a2547bc8727583f9a9db32154b40471
Roland Naijuka <rnaijuka@google.com>
2020-07-28 09:54:07 -0400

    add description to validatable file
    Signed-off-by: Roland Naijuka <rnaijuka@google.com>

--- biogrid/libs/grid-simulator/src/lib/validate-inputs/validate-inputs.ts
+++ biogrid/libs/grid-simulator/src/lib/validate-inputs/validate-inputs.ts
@@ -1,4 +1,18 @@
-// Validation
+/**
+ * The file is used by the gridItems to validate the inputs for the gridItems.
+ * It makes 3 validations for the gridItem inputs.
+ * 1. It ensures that the @param value for the gridItem is not less than the specified 
+ * minimum value @param min for the griditem input
+ * 2. It ensures that the @param value is not greater than the specified maximum 
+ * value @param max for the griditem input
+ * 3. It ensures that the values @param value, @param max, @param min for the gridItem are positive
+ *
+ * @summary the file is used to validable inputs.
+ * @author Roland Naijuka <rnaijuka@google.com>
+ *
+ * Created at     : 6/29/2020, 4:34:42 PM
+ * Last modified  : 7/28/2020, 9:45:42 AM
+ */
 export interface Validatable {
   value: number;
   min?: number;
