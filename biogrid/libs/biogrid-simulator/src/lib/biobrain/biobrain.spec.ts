e122c0631f26376ac40963f2674b2a693fc0593a
RolandNaijuka <rnaijuka@google.com>
2020-07-29 11:42:26 -0400

    update global values

--- biogrid/libs/biogrid-simulator/src/lib/biobrain/biobrain.spec.ts
+++ biogrid/libs/biogrid-simulator/src/lib/biobrain/biobrain.spec.ts
@@ -6,9 +6,9 @@ import { RuralArea } from "../community";
 
 let actualBrain: BioBrain;
 let grid: Biogrid;
-const name1 = `${GRID_ITEM_NAMES.ENERGY_USER}-1`;
-const name2 = `${GRID_ITEM_NAMES.ENERGY_USER}-2`;
-const name3 = `${GRID_ITEM_NAMES.ENERGY_USER}-3`;
+const ENERGY_USER_1 = `${GRID_ITEM_NAMES.ENERGY_USER}-1`;
+const ENERGY_USER_2 = `${GRID_ITEM_NAMES.ENERGY_USER}-2`;
+const ENERGY_USER_3 = `${GRID_ITEM_NAMES.ENERGY_USER}-3`;
 
 beforeAll(() => {
   actualBrain = BioBrain.Instance;
@@ -17,19 +17,19 @@ beforeAll(() => {
       energy: BUILDING.MAX_CAPACITY,
       x: 3,
       y: 4,
-      gridItemName: name1
+      gridItemName: ENERGY_USER_1
     } as BuildingParams),
     new Building({
       energy: 0,
       x: 7,
       y: 9,
-      gridItemName: name2
+      gridItemName: ENERGY_USER_2
     } as BuildingParams),
     new Building({
       energy: BUILDING.MAX_CAPACITY,
       x: 7,
       y: 8,
-      gridItemName: name3
+      gridItemName: ENERGY_USER_3
     } as BuildingParams),
   ];
   grid = new Biogrid(
@@ -46,12 +46,12 @@ describe('BioBrain class', () => {
   test('computeAction sends the required action in form of {receiverName: supplierName}', async () => {
     const action = await actualBrain.computeAction(grid.getSystemState());
     const expectedPath = {
-      [name2] : `${GRID_ITEM_NAMES.LARGE_BATTERY}-0`,
+      [ENERGY_USER_2] : `${GRID_ITEM_NAMES.LARGE_BATTERY}-0`,
     };
     // Expect the supplying paths to be similar to the one above
     const actualPath = action.getSupplyingPaths();
     
-    expect(actualPath[name2]).toEqual(expectedPath[name2]);
+    expect(actualPath[ENERGY_USER_2]).toEqual(expectedPath[ENERGY_USER_2]);
     expect(Object.keys(action.getSupplyingPaths()).length).toBeGreaterThanOrEqual(1);
   });
 });
